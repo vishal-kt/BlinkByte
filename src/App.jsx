f49a8af97325aeb090ae33cd15ff8c545fcecec0
VISHAL <1316510155@kit.ac.in>
2024-03-18 12:24:29 +0530

    blinkbyte

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-
+import React,{useState,useEffect} from 'react'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login,logout } from './store/authSlice';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
 function App() {
-  return (
-    <div>App</div>
-  )
+  // now we need conditional rendering 
+  // because we are fetching data from server 
+  //we can do loading state according to that
+
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser().then(
+      (userData)=>{
+        if(userData){ 
+          dispatch(login({userData}))
+        }else{
+          dispatch(logout( ))
+        }
+      }
+    ).finally(()=>setLoading(false))
+    
+  }, []);
+ 
+  return !loading ?(
+
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block bg-red-500'>
+
+        <Header/>
+        <main>
+          <Outlet/>
+        </main>
+        <Footer/>
+      </div>
+    </div>
+
+  ):null
 }
 
 export default App
